f21e950217e9f63bede6dbfc761d1d93135e10a8
Mario Yepes <marioy47@gmail.com>
2019-12-25 23:43:21 -0500

    Registrando el proyecto como plugin de Wordpress y creando links en el listado de plugins

--- app/Settings/PluginList.php
+++ app/Settings/PluginList.php
@@ -1,1 +1,91 @@
 <?php
+/**
+ * Changes on the Dashboard Plugin List
+ *
+ * @package Wordpress_Custom_Directory
+ */
+
+namespace WpCustomDir\Settings;
+
+/**
+ * Changes the plugin list.
+ */
+class PluginList {
+
+	/**
+	 * Singleton.
+	 */
+	private function __construct() {
+
+	}
+
+	/**
+	 * Factory function
+	 *
+	 * @return self
+	 */
+	public static function factory(): self {
+		static $obj;
+		return isset( $obj ) ? $obj : $obj = new self();
+	}
+
+	/**
+	 * Executes the add_action() and add_filter() functions form WordPress.
+	 *
+	 * @return self
+	 */
+	public function start(): self {
+		add_filter( 'plugin_action_links_' . plugin_basename( $this->plugin_file ), array( $this, 'add_links' ) );
+
+		return $this;
+	}
+
+	/**
+	 * Adds the links.
+	 *
+	 * @param array $links Array of links passed by WordPress.
+	 * @return array
+	 */
+	public function add_links( $links ): array {
+		$links[] = '<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_slug ) . '">' . __( 'Settings', 'wp-custom-dir' ) . '</a>';
+
+		return $links;
+	}
+
+	/**
+	 * Absolute path the to initial plugin file.
+	 *
+	 * @var string
+	 */
+	protected $plugin_file = '';
+
+	/**
+	 * Setter for $this->plugin_file.
+	 *
+	 * @param string $file The path.
+	 * @return self
+	 */
+	public function set_plugin_file( $file ): self {
+		$this->plugin_file = $file;
+		return $this;
+	}
+
+	/**
+	 * Slug used in the plugin links.
+	 *
+	 * @var string
+	 */
+	protected $plugin_slug = '';
+
+	/**
+	 * Setter for $this->plugin_slug.
+	 *
+	 * @param string $slug Short string with no spaces.
+	 * @return self
+	 */
+	public function set_plugin_slug( $slug ): self {
+		$this->plugin_slug = $slug;
+		return $this;
+	}
+
+}
--- wordpress-custom-directory.php
+++ wordpress-custom-directory.php
@@ -1,5 +1,38 @@
 <?php
+/**
+ * WordPress Custom Directory Plugin
+ *
+ * @package           Wordpress_Custom_Directory
+ * @author            Mario Yepes <marioy47@gmail.com>
+ * @copyright         2019 Mario Yepes
+ * @license
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WordPress Custom Directory
+ * Plugin URI:        https://marioyepes.com
+ * Description:       Creates a searcheable directory of people, elements, or anythig a  custom type can be.
+ * Version:           1.0.0
+ * Requires at least: 5.0
+ * Requires PHP:      7.0
+ * Author:            Mairo Yepes
+ * Author URI:        https://marioyepes.com
+ * Text Domain:       wp-custom-dir
+ * License:
+ * License URI:
+ */
 
-function worongNameFunction() {
-	 return false;
+namespace WpCustomDir;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
+
+// For javascript and css file enqueueing.
+define( 'WP_CUSTOM_DIRECTORY_VERSION', '1.0.0' );
+
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Add links to the plugin list.
+Settings\PluginList::factory()->set_plugin_file( __FILE__ )->set_plugin_slug( 'wordpress-custom-directory' )->start();
